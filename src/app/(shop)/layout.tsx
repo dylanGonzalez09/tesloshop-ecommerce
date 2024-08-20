68511aa8a48e158f1f82f3c36403f055ce49113e
Yair Gonzalez <dyg09032003@gmail.com>
2024-08-20 00:00:58 -0500

    initial products

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -4,7 +4,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen">
       <TopMenu />
-      {children}
+      <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
 };
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,14 @@
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hello world 1</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hello world 2</h1>
-    </div>
+    <>
+      <Title title="Tienda" subtitle="todo los productos" className="mb-2" />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
